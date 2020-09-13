549d7393f70902a233d9e20e2931712a0e412eb7
tam2511@mail.ru <tam2511@mail.ru>
2020-09-13 12:17:50 +0300

    gitignore + config

--- config.py
+++ config.py
@@ -11,3 +11,8 @@ Cfg.lr = 0.001
 Cfg.models_dir = os.path.join(_BASE_DIR, 'weights')
 Cfg.confidence_score = 0.8
 Cfg.num_boxes = 10
+Cfg.train_dataset = 'D:\\Data\\fish\\datasets\\fish_in_the_wild\\train'
+Cfg.test_dataset = 'D:\\Data\\fish\\datasets\\fish_in_the_wild\\test'
+
+Cfg.train_labels = 'D:\\Data\\fish\\datasets\\fish_in_the_wild\\train.txt'
+Cfg.test_labels = 'D:\\Data\\fish\\datasets\\fish_in_the_wild\\test.txt'
