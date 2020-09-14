8ec2a3b91d0a7bf26ec72f79ad43fb21dac24d4c
AlecanderOfMacedon <tam2511@mail.ru>
2020-09-14 13:27:07 +0300

    add ResizeKeepDim

--- transforms/transforms_boxes.py
+++ transforms/transforms_boxes.py
@@ -1,5 +1,9 @@
 import torch
 
+import cv2
+
+import numpy as np
+
 import albumentations as A
 
 from torchvision.transforms import functional as F
@@ -77,6 +81,35 @@ class RGBShift(object):
         transformed = self.transform(image=image, bboxes=target['boxes'], category_ids=[0 for _ in target['boxes']])
         target['boxes'] = transformed['bboxes']
         return transformed['image'], target
+
+
+class ResizeKeepDim(object):
+    def __init__(self, weight, height):
+        self.h = height
+        self.w = weight
+
+    def __call__(self, image, target):
+        h, w, c = image.shape
+        if h > self.h and w > self.w:
+            rescale  = min(self.h / h, self.w / w)
+        elif h > self.h:
+            rescale = self.h / h
+        elif w > self.w:
+            rescale = self.w / w
+        else:
+            rescale = 1.0
+        t_image = image.copy()
+        if rescale < 1.0:
+            target['boxes'] *= rescale
+            t_image = cv2.resize(t_image, (int(w * rescale), int(h * rescale)))
+        return self.add_padding_(t_image)
+
+    def add_padding_(self, image):
+        t_image = np.zeros(self.h, self.w, image.shape[2])
+        t_image[:image.shape[0], :image.shape[1],:] = t_image
+        return t_image
+
+
 
 
 class ToTensor(object):
