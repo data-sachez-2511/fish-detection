643886e91b8f28ccedbe82982cb14f0b8dddc035
AlecanderOfMacedon <tam2511@mail.ru>
2020-09-14 15:17:20 +0300

    config

--- config.py
+++ config.py
@@ -18,3 +18,11 @@ Cfg.train_labels = 'D:\\Data\\fish\\datasets\\fish_in_the_wild\\train.txt'
 Cfg.test_labels = 'D:\\Data\\fish\\datasets\\fish_in_the_wild\\test.txt'
 
 Cfg.variance = [0.1, 0.2]
+Cfg.feature_maps = [38, 19, 10, 5, 3, 1]
+Cfg.min_dim = 300
+Cfg.steps = [8, 16, 32, 64, 100, 300]
+Cfg.min_sizes = [30, 60, 111, 162, 213, 264]
+Cfg.max_sizes = [60, 111, 162, 213, 264, 315]
+Cfg.aspect_ratios = [[2], [2, 3], [2, 3], [2, 3], [2], [2]]
+Cfg.clip = True
+Cfg.name = 'VOC'
